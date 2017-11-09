3c100cd26b920e8b3ff70585e49c964b7e053b1d
Mayra Mendez <mmendez9@dtcc.edu>
2017-11-09 06:49:15 -0800

    Download

--- webScrape.py
+++ webScrape.py
@@ -1,15 +1,30 @@
 import webbrowser
 import sys
-import pyperclip
+import requests
+# import pyperclip
 
-if (len(sys.argv) > 1):
-    address = ' '.join(sys.argv[1:])
-else:
-    address = pyperclip.paste()
+# if (len(sys.argv) > 1):
+#     address = ' '.join(sys.argv[1:])
+# else:
+#     address = pyperclip.paste()
 
-address2 = input("Enter second location: ".strip())
-
+# Address for the first location
+address = input("Enter starting address: ".strip())
 webbrowser.open("https://google.com/maps/place/" + address)
 
+# Address for the second location
+address2 = input("Enter second location: ".strip())
 webbrowser.open("https://google.com/maps/place/" + address2)
+
+# Tab for directions from first location to second location
+direction = "https://google.com/maps/dir/" + address + "/" + address2
+webbrowser.open(direction)
+
+# Download the directions page
+directions = requests.get(direction)
+
+try:
+    directions.raise_for_status()
+except Exception as exc:
+    print('There was a problem: %s' %(exc))
 
