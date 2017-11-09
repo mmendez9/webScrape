cb4de49466cb5144d79eebe3a7544524a9d425b1
student <mmendez9@dtcc.edu>
2017-11-09 10:12:07 -0500

    Download

--- webScrape.py
+++ webScrape.py
@@ -1,23 +1,18 @@
 import webbrowser
-import sys
 import requests
-# import pyperclip
-
-# if (len(sys.argv) > 1):
-#     address = ' '.join(sys.argv[1:])
-# else:
-#     address = pyperclip.paste()
 
 # Address for the first location
 address = input("Enter starting address: ".strip())
-webbrowser.open("https://google.com/maps/place/" + address)
 
 # Address for the second location
 address2 = input("Enter second location: ".strip())
-webbrowser.open("https://google.com/maps/place/" + address2)
 
 # Tab for directions from first location to second location
 direction = "https://google.com/maps/dir/" + address + "/" + address2
+
+# Open the browser tabs
+webbrowser.open("https://google.com/maps/place/" + address)
+webbrowser.open("https://google.com/maps/place/" + address2)
 webbrowser.open(direction)
 
 # Download the directions page
@@ -27,4 +22,11 @@ try:
     directions.raise_for_status()
 except Exception as exc:
     print('There was a problem: %s' %(exc))
+
+file = open("directions.txt", 'wb')
+
+for data in directions.iter_content(100000):
+    file.write(data)
+file.close()
+
 
